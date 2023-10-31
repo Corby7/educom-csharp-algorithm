cf4bee6605ab1a2f91b44c29b2d8279412351776
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-31 16:43:42 +0100

    created partition function #2

--- Organizer/RotateSort.cs
+++ Organizer/RotateSort.cs
@@ -15,10 +15,10 @@ namespace Organizer
         /// <returns>The sorted array</returns>
         public List<int> Sort(List<int> input)
         {
-            array = new List<int>(input);
+            this.array = new List<int>(input);
 
             SortFunction(0, array.Count - 1);
-            return array;
+            return this.array;
         }
 
         /// <summary>
@@ -28,7 +28,15 @@ namespace Organizer
         /// <param name="high">De index within this.array to stop with</param>
         private void SortFunction(int low, int high)
         {
-            throw new NotImplementedException();
+            if (high <= low)
+            {
+                return; // stop if 1 or less items in array
+            }
+
+            int pivot = Partitioning(low, high);
+            SortFunction(low, pivot - 1);
+            SortFunction(pivot + 1, high);
+
         }
 
         /// 
@@ -39,7 +47,27 @@ namespace Organizer
         /// <returns>The index in the array of the first of the 'high' digits</returns>
         private int Partitioning(int low, int high)
         {
-            throw new NotImplementedException();
+            int pivot = this.array[high];
+            int i = low - 1;
+
+            for (int j = low; j <= high - 1; j++) 
+            {
+                if (this.array[j] < pivot)
+                {
+                    i++;
+                    // swap values if smaller than pivot
+                    int temp = this.array[i];
+                    this.array[i] = this.array[j];
+                    this.array[j] = temp;
+                }
+
+                i++; // now at position of pivot (i + 1)
+                int pivotSpot = this.array[i];
+                array[i] = this.array[high];
+                this.array[high] = pivotSpot; // places the pivot (which was at the end of the array) on its correct pivotspot
+            }
+
+            return i; 
         }
     }
 }
