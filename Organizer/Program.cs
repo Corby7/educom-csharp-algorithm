8f4731600476a050a22885e5a830fe5e84eeaf17
Corby7 <corbijn.bulsink@hotmail.com>
2023-11-01 15:21:01 +0100

    added stopwatch, changed randomlist name, changed max count to 200, added prompt for amount #2

--- Organizer/Program.cs
+++ Organizer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Organizer
 {
@@ -8,21 +9,46 @@ namespace Organizer
         public static void Main(string[] args)
         {
             // Press <F5> to run this code, when "Hello World!" appears in a black box, remove the line below and write your code below.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter amount of list items:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
             ShowList("Example of ShowList", new List<int>() { -33, 3, 2, 2, 3, 34, 34, 32, 1, 3, 5, 3, -22, -99, 33, -22, 11, 3, 33, 12, -2, -21, 4, 34, 22, 15, 34,-22 });
 
+            
 
-            List<int> unsortedRandomList = RandomList(12);
+            List<int> unsortedRandomList = GenerateRandomList(amount);
             ShowList("Unsorted RandomList", unsortedRandomList);
 
+            // First sorting method
             ShiftHighestSort shiftHighestSort = new ShiftHighestSort();
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
             List<int> sortedRandomList1 = shiftHighestSort.Sort(unsortedRandomList);
+            stopWatch.Stop();
+            var timeElapsed1 = stopWatch.Elapsed;
+
             ShowList("Sorted RandomList (shift highest sort)", sortedRandomList1);
 
+            // Second sorting method
             RotateSort rotateSort = new RotateSort();
+
+            stopWatch.Restart();
             List<int> sortedRandomList2 = rotateSort.Sort(unsortedRandomList);
+            stopWatch.Stop();
+            var timeElapsed2 = stopWatch.Elapsed;
+
             ShowList("Sorted RandomList (rotate sort)", sortedRandomList2);
 
+            // Write down stopwatch time
+
+            Console.WriteLine();
+            Console.WriteLine("Stopwatch time first sorting method (shift highest sort): " + timeElapsed1);
+            Console.WriteLine();
+            Console.WriteLine("Stopwatch time second sorting method (rotate sort): " + timeElapsed2);
+            Console.WriteLine();
+
+            // Check first sorting method
             Console.WriteLine();
             if (CheckSort(unsortedRandomList))
             {
@@ -58,7 +84,7 @@ namespace Organizer
             return true;
         }
 
-        public static List<int> RandomList(int amount)
+        public static List<int> GenerateRandomList(int amount)
         {
     
             var randomList = new List<int>();
@@ -86,7 +112,7 @@ namespace Organizer
             int count = theList.Count;
             if (count > 100)
             {
-                count = 300; // Do not show more than 300 numbers
+                count = 200; // Do not show more than 300 numbers
             }
 
             Console.WriteLine();
