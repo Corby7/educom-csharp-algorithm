dc55d29866d2dedbb62ebe4764a4ee968e676fba
Corby7 <corbijn.bulsink@hotmail.com>
2023-11-02 14:44:31 +0100

    added constructor #2

--- BornToMove/Move.cs
+++ BornToMove/Move.cs
@@ -12,6 +12,15 @@ namespace BornToMove
         public string Name { get; set; }
         public string Description { get; set; }
         public int SweatRate { get; set; }
+
+        public Move(int id, string name, string description, int sweatrate)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            SweatRate = sweatrate;
+        }
+            
     }
 }
 
