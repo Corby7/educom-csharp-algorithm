fad009bb3a651af10e24cdcb14af05cbc2bcfd7c
Corby7 <corbijn.bulsink@hotmail.com>
2023-11-02 14:45:24 +0100

    added displayExercises function, moved some code around #2

--- BornToMove/Program.cs
+++ BornToMove/Program.cs
@@ -10,11 +10,7 @@ namespace BornToMove
         {
             Console.WriteLine();
             var connection = new DBConnect();
-            int id = Randomizer(connection);
-            Console.WriteLine("Randomly selected exercise ID: " + id);
-
-
-
+            
             bool validOptionSelected = false;
 
             Console.WriteLine("Hi there! You have been inactive for too long, it's time to start moving!");
@@ -35,18 +31,37 @@ namespace BornToMove
 
                     switch (key)
                     {
-                        case 1:
-                            Console.WriteLine("You entered, " + key + " as your login key!");
+                        case 1: // Exercise suggestion
+                            Console.WriteLine("You opted for an exercise suggestion (option [1])");
                             validOptionSelected = true;
+
+                            int id = Randomizer(connection);
+                            Console.WriteLine();
+                            Console.WriteLine("Randomly selected exercise ID: " + id);
+                            Console.WriteLine();
+                            Dictionary<int, Move> exerciseArray = connection.getExercise(id);
+                            DisplayExercises(exerciseArray);
+                            Console.WriteLine();
+
                             break;
-                        case 2:
-                            Console.WriteLine("You entered, " + key + " as your login key!");
+
+                        case 2: // Choose from list
+                            Console.WriteLine("You opted to choose from a list with exercies (option [2])");
                             validOptionSelected = true;
+                            Console.WriteLine();
                             break;
+
                         default:
                             Console.WriteLine("Please choose option 1 or 2, try again.");
+                            Console.WriteLine();
                             break;
                     }
+
+                    //nog functionaliteit toevoegen
+                    Console.WriteLine("Congratulations, you finished the exercise!");
+                    Console.WriteLine("On a scale from 1-5, how do you rate this exercise?");
+                    Console.WriteLine("And on a scale from 1-5, how intense was this exercise?");
+                    Console.WriteLine();
                 } 
                 else
                 {
@@ -71,6 +86,21 @@ namespace BornToMove
             else
             {
                 return -1; // if no values in Ids
+            }
+        }
+
+        public static void DisplayExercises(Dictionary<int, Move> exerciseArray)
+        {
+            foreach (var kvp in exerciseArray)
+            {
+                int exerciseId = kvp.Key;
+                Move exercise = kvp.Value;
+
+                Console.WriteLine($"Suggested Exercise: { exerciseId}");
+                Console.WriteLine($"Name: {exercise.Name}");
+                Console.WriteLine($"Description: {exercise.Description}");
+                Console.WriteLine($"Sweat Rate: {exercise.SweatRate}");
+                Console.WriteLine();
             }
         }
 
