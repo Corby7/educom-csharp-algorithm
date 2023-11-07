2257a6182c8bf15db4e2372180670c5306b43c0c
Corby7 <corbijn.bulsink@hotmail.com>
2023-11-07 12:23:45 +0100

    added nameExists, readAllId's functions. Added errorhandling #4

--- BornToMove.DAL/MoveCrud.cs
+++ BornToMove.DAL/MoveCrud.cs
@@ -18,38 +18,101 @@ namespace BornToMove.DAL
             this.context = context;
         }
 
-        public void createMove(Move newmove)
+        public void createMove(Move newMove)
         {
-            context.Move.Add(newmove);
-            context.SaveChanges();
+            try
+            {
+                context.Move.Add(newMove);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while creating a move: " + ex.Message);
+            }
         }
 
         public void updateMove(Move updatedMove)
         {
-            context.Move.Update(updatedMove);
-            context.SaveChanges();
+            try
+            {
+                context.Move.Update(updatedMove);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while updating a move: " + ex.Message);
+            }
         }
 
         public void deleteMove(int id)
         {
-            var movement = context.Move.Find(id);
-
-            if (movement != null )
+            try
             {
-                context.Move.Remove(movement);
-                context.SaveChanges();
+                var movement = context.Move.Find(id);
+
+                if (movement != null)
+                {
+                    context.Move.Remove(movement);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while deleting a move: " + ex.Message);
             }
         }
 
         public Move? readMoveById(int id)
         {
-            return context.Move.Find(id);
+            try
+            {
+                return context.Move.Find(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while reading a move by ID: " + ex.Message);
+                return null;
+            }
         }
 
 
         public Dictionary<int, Move>? readAllMoves()
         {
-            return context.Move.ToDictionary(move => move.Id, move => move);         
+            try
+            {
+                return context.Move.ToDictionary(move => move.Id, move => move);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while reading all moves: " + ex.Message);
+                return null;
+            }
+        }
+
+        public List<int>? readMoveIds()
+        {
+            try
+            {
+                return context.Move.Select(move => move.Id).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while reading move IDs: " + ex.Message);
+                return null;
+            }
+        }
+
+        public bool nameExists(string name)
+        {
+            try
+            {
+                return context.Move.Any(move => move.Name == name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while checking if a name exists: " + ex.Message);
+                return false;
+            }
         }
 
 
