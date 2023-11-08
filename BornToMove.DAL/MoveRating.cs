0dba1032bac6980ca7cc4e30722bad8772ad4a47
Corby7 <corbijn.bulsink@hotmail.com>
2023-11-08 16:03:12 +0100

    created & added moveRating #5

---
+++ BornToMove.DAL/MoveRating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BornToMove.DAL
+{
+    public class MoveRating
+    {
+        public int Id { get; set; }
+
+        public Move Move { get; set; }
+
+        public double Rating { get; set; }
+
+        public double Vote { get; set; }
+
+
+
+    }
+}
